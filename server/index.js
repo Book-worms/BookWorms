8d021fdc2b6b77c0934bdc0dd074aa603c847606
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-19 11:35:51 -0500

    changed port to 80 for digital ocean

--- server/index.js
+++ server/index.js
@@ -264,7 +264,7 @@ app.get('/goodreads', (req, res) => {
 
 
 // Set Port, hosting services will look for process.env.PORT
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 80));
 
 // start the server
 app.listen(app.get('port'), () => {
