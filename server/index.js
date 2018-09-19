585c541a34b2b34cb2956cc06b16381f3bea259e
jvalamis <jvalamis@gmail.com>
2018-09-19 16:35:18 -0500

    ebayfunction

--- server/index.js
+++ server/index.js
@@ -271,7 +271,7 @@ app.get('/goodreads', (req, res) => {
 
 // Set Port, hosting services will look for process.env.PORT
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 3000));
 // start the server
 app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${app.get('port')}`);
