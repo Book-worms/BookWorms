1fc877ba6014e9cf82a3b9d6cc105210fd19714f
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-19 17:25:57 -0500

    completed ebayHelpers

--- server/helpers.js
+++ server/helpers.js
@@ -7,8 +7,6 @@ require('dotenv').config();
 const goodReads = process.env.GOODREADS_KEY;
 const google = process.env.GOOGLE;
 const libThing = process.env.LIBRARYTHING;
-//create variable to access ebay_app_id env
-const ebay = process.env.EBAY_APP_ID;
 
 
 
@@ -27,8 +25,7 @@ const openLibrary = isbn => axios.get(`https://openlibrary.org/api/books.json?bi
 const googleGenre = genre => axios.get(`https://www.googleapis.com/books/v1/volumes?q=${genre}&key=${google}&maxResults=40`);
 
 const libThingISBN = ISBN => axios.get(`http://www.librarything.com/services/rest/1.1/?method=librarything.ck.getwork&isbn=${ISBN}&apikey=${libThing}`);
-//create function to access ebay find api
-const ebayFind = () => axios.get(`http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=a%20song%20of%20ice%20and%20fire&paginationInput.entriesPerPage=3`);
+
 
 module.exports = {
   goodReadsData,
