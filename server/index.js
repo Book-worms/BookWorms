516b3843d1489be9ecb895b377c1699ac2288765
jvalamis <jvalamis@gmail.com>
2018-09-19 13:59:14 -0500

    mongo connected remotely, local node running

--- server/index.js
+++ server/index.js
@@ -264,7 +264,7 @@ app.get('/goodreads', (req, res) => {
 
 
 // Set Port, hosting services will look for process.env.PORT
-app.set('port', (process.env.PORT || 80));
+app.set('port', (3000 || 80));
 
 // start the server
 app.listen(app.get('port'), () => {
