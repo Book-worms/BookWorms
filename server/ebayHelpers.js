edcc907b015d61ed7c6dc156ab996e4c813eaa7b
jvalamis <jvalamis@gmail.com>
2018-09-20 09:48:58 -0500

    git cache cleared

--- server/ebayHelpers.js
+++ server/ebayHelpers.js
@@ -1,21 +1,25 @@
 const request = require('request');
 
-//create variable to access ebay_app_id env
+// create variable to access ebay_app_id env
 const ebay = process.env.EBAY_APP_ID;
 const createKeyWordForSearch = keyword => encodeURI(keyword);
 
-//create function to access ebay find api
+// create function to access ebay find api
 const ebayPost = (keyWordSearch, callback) => {
   const options = {
     method: 'POST',
-    url: `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${keyWordSearch}&paginationInput.entriesPerPage=3`
+    url: `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${keyWordSearch}%book&paginationInput.entriesPerPage=3`,
   };
   request(options, callback);
 };
 
 const ebayHelpers = {
   createKeyWordForSearch,
-  ebayPost
-}
+  ebayPost,
+};
+
+const whyWontYouAdd = {
+  cup: 'coffee',
+};
 
 module.exports.ebayHelpers = ebayHelpers;
