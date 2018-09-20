25290a82689ca52beeee6b5bad28f245f65f9d1f
jvalamis <jvalamis@gmail.com>
2018-09-20 09:43:58 -0500

    test

--- server/ebayhelpers.js
+++ server/ebayhelpers.js
@@ -8,7 +8,7 @@ const createKeyWordForSearch = keyword => encodeURI(keyword);
 const ebayPost = (keyWordSearch, callback) => {
   const options = {
     method: 'POST',
-    url: `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${keyWordSearch}&paginationInput.entriesPerPage=3`,
+    url: `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${keyWordSearch}%book&paginationInput.entriesPerPage=3`,
   };
   request(options, callback);
 };
