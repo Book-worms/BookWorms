955990aa26dee631fc7a4b063a6819b4ea59ba65
jvalamis <jvalamis@gmail.com>
2018-09-20 13:53:21 -0500

    linting error fixed in ebayhelpers

--- server/ebayHelpers.js
+++ server/ebayHelpers.js
@@ -5,7 +5,7 @@ const ebay = process.env.EBAY_APP_ID;
 
 const ebayPost = (title) => {
   const encodedTitle = `${title}`;
-  return axios.get(`http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${encodedTitle}&paginationInput.entriesPerPage=3`)
+  return axios.get(`http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${encodedTitle}&paginationInput.entriesPerPage=3`);
 };
 
 const ebayHelpers = {
