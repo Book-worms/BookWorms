697072ce4f81b51c04559711540505860b09e75c
jvalamis <jvalamis@gmail.com>
2018-09-20 13:53:59 -0500

    fixed search string

--- server/ebayHelpers.js
+++ server/ebayHelpers.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const ebay = process.env.EBAY_APP_ID;
 
 const ebayPost = (title) => {
-  const encodedTitle = `${title}`;
+  const encodedTitle = `${title} book`;
   return axios.get(`http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${ebay}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD=&keywords=${encodedTitle}&paginationInput.entriesPerPage=3`);
 };
 
